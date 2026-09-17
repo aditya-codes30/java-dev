9d1cf1f862974e1cbf5ab6dc38511238d24ba317
Aditya <aditya5j2008y@gmail.com>
2026-09-17 23:09:51 +0530

    java
    new start with java

---
+++ basic/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // i don't know about this yet 
+
+public class input {
+    public static void main(String[] args ){
+     Scanner input = new Scanner (System.in); 
+     System.out.println("please enter your age here without any worry  ");
+     int rollno = input.nextInt();
+     System.out.println("so your age is " + rollno );
+    
+    }
+}
